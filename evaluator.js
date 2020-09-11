2f95b0938310d1608b63624bc29a463db859f5c4
Aleks Mitrovic <mitrovic.aleksandar@outlook.com>
2020-09-11 10:42:09 -0400

    added shell structure for evaluator

--- evaluator.js
+++ evaluator.js
@@ -2,4 +2,16 @@ export default class Evaluator {
   constructor(parseTree) {
     this.parseTree = parseTree;
   }
+  parse(node) {
+    //code goes here
+  }
+
+  evaluate() {
+    let output = '';
+    for (let i = 0; i < this.parseTree.length; i++) {
+      let value = this.parse(this.parseTree[i]);
+      typeof value !== 'undefined' ? (output += value + '\n') : '';
+    }
+    return output;
+  }
 }
