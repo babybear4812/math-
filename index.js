c9d8e5a62fdef2cbe5fa4f653c7952acbfbec623
Aleks Mitrovic <mitrovic.aleksandar@outlook.com>
2020-10-15 14:05:33 -0400

    refactoring code into one index file

--- index.js
+++ index.js
@@ -1,3 +1,53 @@
-import Lexer from './lexer';
-import Parser from './parser';
-import Evaluator from './evaluator';
+// import Lexer from './lexer';
+// import Parser from './parser';
+// import Evaluator from './evaluator';
+
+// All code in one file
+
+class MathPlusPlus {
+  constructor(tokens = []) {
+    this.tokens = tokens;
+  }
+
+  lexer(input) {
+    let tokens = [];
+    function isNumber() {
+      return input.match(/[0-9]/);
+    }
+
+    function isOperator() {
+      return input.match(/[+\-*\/\^%=()]/);
+    }
+
+    function isIdentifier() {
+      //check if the character is a string but not whitespace
+      return typeof input === 'string' && !input.match(/[\s]/);
+    }
+
+    function advance() {
+      //
+    }
+
+    function addToken(type, value) {
+      this.tokens.push({
+        type,
+        value,
+      });
+    }
+
+    while (idx < input.length) {
+      c = input[idx];
+      if (c.match(/[\s]/)) {
+        advance();
+      } else if (isOperator(c)) {
+        addToken('operator', c);
+      } else if (isDigit(c)) {
+        //
+      } else if (isIdentifier(c)) {
+        //
+      }
+    }
+
+    return tokens;
+  }
+}
